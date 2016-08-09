e16de8d15bea5fe76f7e8388c70071710d3627d5
yashwin <yashwinpoojary@gmail.com>
2016-08-10 00:54:19 +0530

    k-count

--- app/models/repo.js
+++ app/models/repo.js
@@ -10,5 +10,35 @@ export default DS.Model.extend({
   stargazers_count: DS.attr(),
   watchers_count: DS.attr(),
   forks_count: DS.attr(),
-  language: DS.attr()
+  language: DS.attr(),
+
+  stars_k_count: Ember.computed('stargazers_count',function(){
+      let num = this.get('stargazers_count');
+      if(parseInt(num,10) > 1000) {
+        return `${Math.round(num * 0.01) * 0.1}K`;
+      } else {
+        return `${num}`;
+      }
+          return `${Math.round(num * 0.01) * 0.1}K`;
+      }),
+  watchers_k_count: Ember.computed('watchers_count',function(){
+          let num = this.get('watchers_count');
+          if(parseInt(num,10) > 1000) {
+            return `${Math.round(num * 0.01) * 0.1}K`;
+          } else {
+            return `${num}`;
+          }
+              return `${Math.round(num * 0.01) * 0.1}K`;
+          }),
+
+  forks_k_count: Ember.computed('forks_count',function(){
+              let num = this.get('forks_count');
+              if(parseInt(num,10) > 1000) {
+                return `${Math.round(num * 0.01) * 0.1}K`;
+              } else {
+                return `${num}`;
+              }
+                  return `${Math.round(num * 0.01) * 0.1}K`;
+              })
+
 });
